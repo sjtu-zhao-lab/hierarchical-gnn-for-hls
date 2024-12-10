8e05f26948677d9d2db1d1e04e02ae2e94513e83
MZccc-X <gao.mingzhe0808@gmail.com>
2024-12-10 15:19:43 +0800

    update

--- utils/llvm-pass/benchmarks/run.sh
+++ utils/llvm-pass/benchmarks/run.sh
@@ -1,3 +1,2 @@
 clang -O1 -emit-llvm -S -fno-discard-value-names test.c -o test.ll
-
-opt -load  ../build/loop_II/libMyPass.so -loopII test.ll -o test-opt.ll
+opt -load  ../build/loop_II/libMyPass.so -loopII -config="A_1_8_A_2_16_tmp_1_8_x_1_16" test.ll -o test-opt.ll

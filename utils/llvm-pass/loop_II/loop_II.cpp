f4ac5c417cfac692b3510811b01db599e536814b
MZccc-X <gao.mingzhe0808@gmail.com>
2024-12-10 15:19:29 +0800

    fix bugs

--- utils/llvm-pass/loop_II/loop_II.cpp
+++ utils/llvm-pass/loop_II/loop_II.cpp
@@ -17,15 +17,21 @@
 #include "llvm/Analysis/TargetTransformInfo.h"
 #include "llvm/Analysis/DependenceAnalysis.h"
 #include "llvm/Analysis/ScalarEvolutionExpressions.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Analysis/AliasAnalysis.h"
 #include <cstdlib>
 #include <tuple>
 #include <map> 
 #include <queue>
 #include <set>
+#include <sstream>
 
 using namespace llvm;
 
 #define DEBUG_TYPE "loopII"
+
+static cl::opt<std::string> arrayConfig("config", cl::desc("Specify array configuration string"), cl::value_desc("string"));
+
 
 std::vector<size_t> arrayPartition[30] = {{1}, {1, 2}, {2}, {1}, {1}, {1}, {1}};
 static size_t arrayCnt = 0;
@@ -63,16 +69,14 @@ Value* getArrayName(Instruction* inst) {
 	return arrayName;
 }
 
-size_t calcPorts(Value* arrayName) {
-  size_t arrayIdx = arrayNum[arrayName];
-  std::vector<size_t>& arrayPartitionFactor = arrayPartition[arrayIdx];
-  size_t fact = 1;
-  for (auto f: arrayPartitionFactor)
-    fact *= f;
-  return fact; 
+size_t calcPorts(std::string arrayNameStr, std::map<std::string, int>& arrayConfig) {
+  if (arrayConfig.find(arrayNameStr) == arrayConfig.end()) {
+    return 1;
+  }
+  return arrayConfig[arrayNameStr]; 
 }
 
-size_t computeCriticalPath(std::vector<std::vector<size_t>>& G, std::vector<size_t>& w, size_t st, size_t ed) {
+size_t computeCriticalPath(std::vector<std::vector<size_t>>& G, std::vector<size_t>& w, size_t st, size_t ed, bool verbose = false) {
   size_t nodeNum = G.size();
   std::vector<size_t> d(nodeNum);
   std::vector<size_t> inq(nodeNum);
@@ -97,6 +101,7 @@ size_t computeCriticalPath(std::vector<std::vector<size_t>>& G, std::vector<size
   while (!q.empty()) {
     size_t u = q.front(); 
     q.pop();
+    inq[u] = false;
     for (auto v: G[u]) {
       if (d[v] < d[u] + w[v]) {
         d[v] = d[u] + w[v];
@@ -183,11 +188,32 @@ size_t calcDistance(Instruction* loadInst, Instruction* storeInst) {
   return 1;
 }
 
-size_t calcResMII(Loop* L) {
+std::string findArrayName(Value* val) {
+  std::string str;
+  llvm::raw_string_ostream rso(str);
+  val->print(rso);
+  size_t pos = rso.str().find('%');
+  // errs() << "arrayName: " << rso.str().substr(pos + 1, rso.str().size() - pos - 1) << "\n";
+  return rso.str().substr(pos + 1, rso.str().size() - pos - 1);
+}
+
+std::string findOffset(Value* val) {
+  std::string str;
+  llvm::raw_string_ostream rso(str);
+  val->print(rso);
+  std::string fullStr = rso.str();
+  size_t pos = fullStr.find('=');
+  if (pos != std::string::npos) {
+    return fullStr.substr(pos + 1); // 提取等号后面的部分
+  }
+  return fullStr;
+}
+
+size_t calcResMII(Loop* L, std::map<std::string, int>& arrayConfig) {
   std::map<Value*, size_t> numLoad;
   std::map<Value*, size_t> numStore;
-  std::map<Value*, std::set<Value*>> loadOffset;
-  std::map<Value*, std::set<Value*>> storeOffset;
+  std::map<Value*, std::set<std::string>> loadOffset;
+  std::map<Value*, std::set<std::string>> storeOffset;
 
   for (auto &L_bb: L->getBlocks()) {
     for (auto &I: *L_bb) {
@@ -195,17 +221,18 @@ size_t calcResMII(Loop* L) {
         Value* arrayName = getArrayName(&I);
         if (arrayNum.find(arrayName) == arrayNum.end()) {
           arrayNum[arrayName] = arrayCnt++;
-          loadOffset[arrayName] = std::set<Value*>();
+          loadOffset[arrayName] = std::set<std::string>();
           numLoad[arrayName] = 0;
-          errs() << arrayCnt - 1 << " " << *arrayName << "\n";
+          // errs() << arrayCnt - 1 << " " << *arrayName << "\n";
         }
         LoadInst *ldInst = dyn_cast<LoadInst>(&I);
         Value* ldAddr = ldInst->getPointerOperand();
         auto& offsetSet = loadOffset[arrayName];
-        if (offsetSet.find(ldAddr) == offsetSet.end()) {
-          //errs() << *arrayName << " " << *ldAddr << "\n\n";
+        std::string offsetStr = findOffset(ldAddr);
+        if (offsetSet.find(offsetStr) == offsetSet.end()) {
+          // errs() << *arrayName << " " << *ldAddr << "\n";
           ++numLoad[arrayName];
-          offsetSet.insert(ldAddr);
+          offsetSet.insert(offsetStr);
         }
         //errs() << "Load: " << *arrayName << "\n";
       }
@@ -214,16 +241,17 @@ size_t calcResMII(Loop* L) {
         if (arrayNum.find(arrayName) == arrayNum.end()) {
           //errs() << arrayCnt << ": " << *arrayName << "\n";
           arrayNum[arrayName] = arrayCnt++;
-          storeOffset[arrayName] = std::set<Value*>();
+          storeOffset[arrayName] = std::set<std::string>();
           numStore[arrayName] = 0;
-          errs() << arrayCnt - 1 << " " << *arrayName << "\n";
+          // errs() << arrayCnt - 1 << " " << *arrayName << "\n";
         }
         StoreInst *stInst = dyn_cast<StoreInst>(&I);
         Value* stAddr = stInst->getPointerOperand();
         auto& offsetSet = storeOffset[arrayName];
-        if (offsetSet.find(stAddr) == offsetSet.end()) {
+        std::string offsetStr = findOffset(stAddr);
+        if (offsetSet.find(offsetStr) == offsetSet.end()) {
           ++numStore[arrayName];
-          offsetSet.insert(stAddr);
+          offsetSet.insert(findOffset(stAddr));
         }
         //errs() << "Store: " << *arrayName << "\n";
       }
@@ -232,8 +260,9 @@ size_t calcResMII(Loop* L) {
 
   size_t loadII = 1;
   for (auto [arrayName, loadAccess]: numLoad) {
-    loadII = std::max(loadII, loadAccess / calcPorts(arrayName));
-    errs() << *arrayName << " " << loadAccess << " " << calcPorts(arrayName) << "\n";
+    int ports = calcPorts(findArrayName(arrayName), arrayConfig);
+    loadII = std::max(loadII, loadAccess % ports == 0 ? loadAccess / ports : loadAccess / ports + 1);
+    // errs() << *arrayName << " " << loadAccess << " " << ports << "\n";
   }
 
   /*
@@ -263,7 +292,51 @@ bool checkType(Instruction* I) {
       || I->getOpcode() == Instruction::FMul || I->getOpcode() == Instruction::Add || I->getOpcode() == Instruction::Mul;
 }
 
-size_t calcRecMII(Loop* L, LoopInfo* LI, DependenceInfo* depInfo) {
+bool hasSameOffset(GetElementPtrInst *GEP1, GetElementPtrInst *GEP2) {
+  if (GEP1->getNumOperands() != GEP2->getNumOperands()) {
+    return false;
+  }
+
+  for (unsigned i = 1; i < GEP1->getNumOperands(); ++i) { // 从1开始，跳过基指针
+    if (GEP1->getOperand(i) != GEP2->getOperand(i)) {
+      // errs() << "GEP1: " << *GEP1->getOperand(i) << " GEP2: " << *GEP2->getOperand(i) << "\n";
+      return false;
+    }
+  }
+  return true;
+}
+
+bool isSameMemoryBlock(Value* srcAddr, Value* desAddr) {
+  bool memDep = false;
+  Value *srcPtr = nullptr;
+  Value *desPtr = nullptr;
+
+  if (auto *srcLoad = dyn_cast<LoadInst>(srcAddr)) {
+    srcPtr = srcLoad->getPointerOperand();
+  } else if (auto *srcStore = dyn_cast<StoreInst>(srcAddr)) {
+    srcPtr = srcStore->getPointerOperand();
+  }
+
+  if (auto *desLoad = dyn_cast<LoadInst>(desAddr)) {
+    desPtr = desLoad->getPointerOperand();
+  } else if (auto *desStore = dyn_cast<StoreInst>(desAddr)) {
+    desPtr = desStore->getPointerOperand();
+  }
+
+  if (srcPtr && desPtr) {
+    if (auto *srcGEP = dyn_cast<GetElementPtrInst>(srcPtr)) {
+      if (auto *desGEP = dyn_cast<GetElementPtrInst>(desPtr)) {
+        // errs() << "srcGEP: " << *srcGEP << " desGEP: " << *desGEP << "\n";
+        memDep = hasSameOffset(srcGEP, desGEP);
+      }
+    } else {
+      memDep = (srcPtr == desPtr);
+    }
+  }
+  return memDep;
+}
+
+size_t calcRecMII(Loop* L, LoopInfo* LI, DependenceInfo* depInfo, AliasAnalysis &AA) {
   size_t recMII = 1;
 
   // build CFG
@@ -299,44 +372,99 @@ size_t calcRecMII(Loop* L, LoopInfo* LI, DependenceInfo* depInfo) {
     }
   }
 
+  int memLinkCnt = 0;
+
   for (size_t i = 0; i < instNum; ++i) {
     auto src_I = instArray[i];
     for (int j = i + 1; j < instNum; ++j) {
       auto des_I = instArray[j];
+      
       bool dep = dataDep(src_I, des_I);
-      if (dep && checkType(src_I) && checkType(des_I)) {
-        //errs() << instMap[src_I] << " -> " << instMap[des_I] << "\n";
+      
+      bool memDep = false;
+      if (src_I->mayReadOrWriteMemory() && des_I->mayReadOrWriteMemory()) {
+        memDep = isSameMemoryBlock(src_I, des_I);
+      }
+      
+      if ((dep || memDep) && checkType(src_I) && checkType(des_I) &&
+          !(isa<LoadInst>(src_I) && isa<LoadInst>(des_I)) &&
+          !(isa<StoreInst>(src_I) && isa<StoreInst>(des_I))) {
         G[i].push_back(j);
+        
+        //errs() << "dep: " << *src_I << " -> " << *des_I << "\n";
+        
       }
     }
   }
 
+  int cnt = 0;
+  
   for (auto &L_bb: L->getBlocks()) {
     for (auto &src_I: *L_bb) {
       if (src_I.getOpcode() == Instruction::Load) {
+        Value *srcAddr = dyn_cast<LoadInst>(&src_I)->getPointerOperand();
         for (auto &des_I: *L_bb) {
           if (des_I.getOpcode() == Instruction::Store) {
-            std::unique_ptr<Dependence> infoPtr;
-            infoPtr = depInfo->depends(&src_I, &des_I, true);
-            Dependence *dep = infoPtr.get();
-            if (dep != NULL && !dep->isInput() && !dep->isConfused()) {
-              size_t src = instMap[&src_I], des = instMap[&des_I];
-              errs() << src_I << " -> " << des_I << "\n";
+            Value *desAddr = dyn_cast<StoreInst>(&des_I)->getPointerOperand();
 
-              size_t delay = computeCriticalPath(G, w, src, des);
-              errs() << "delay: " << delay << "\n";
+            bool memDep = isSameMemoryBlock(&src_I, &des_I);
 
-              size_t distance = calcDistance(&src_I, &des_I);
-              errs() << "distance: " << distance << "\n";
+            if (memDep) {
+              std::unique_ptr<Dependence> infoPtr;
+              infoPtr = depInfo->depends(&src_I, &des_I, true);
+              Dependence *dep = infoPtr.get();
+              if (dep != NULL && !dep->isInput() && !dep->isConfused()) {
+                ++cnt;
+                
+                size_t src = instMap[&src_I], des = instMap[&des_I];
+                // errs() << src_I << " -> " << des_I << "\n";
 
-              recMII = std::max(recMII, static_cast<size_t>(ceil(1.0 * delay / distance)));
+                size_t delay = 0;
+                delay = computeCriticalPath(G, w, src, des);
+                // errs() << "delay: " << delay << "\n";
+
+                size_t distance = calcDistance(&src_I, &des_I);
+                // errs() << "distance: " << distance << "\n";
+
+                recMII = std::max(recMII, static_cast<size_t>(ceil(1.0 * delay / distance)));
+              }
             }
           }  
         }
       }
     }
   }
+  
   return recMII;
+}
+
+auto parseConfig(std::string config) {
+  //config: A_1_2_A_2_1_tmp_1_2
+  //返回map<string, pair<int, int>>
+  std::map<std::string, int> arrayConfigMap;
+  std::istringstream ss(config);
+  std::string token;
+  std::vector<std::string> tokens;
+
+  // 分割字符串
+  while (std::getline(ss, token, '_')) {
+    tokens.push_back(token);
+  }
+
+  // 解析并存储到 map 中
+  for (size_t i = 0; i < tokens.size(); i += 3) {
+    if (i + 2 < tokens.size()) {
+      std::string name = tokens[i];
+      int first = std::stoi(tokens[i + 1]);
+      int second = std::stoi(tokens[i + 2]);
+      if (arrayConfigMap.find(name) == arrayConfigMap.end()) {
+        arrayConfigMap[name] = second;
+      } else {
+        arrayConfigMap[name] *= second;
+      }
+    }
+  }
+  return arrayConfigMap;
 }
 
 namespace {
@@ -358,6 +486,7 @@ namespace {
       AU.addPreserved<ScalarEvolutionWrapperPass>();
       AU.addRequired<DependenceAnalysisWrapperPass>();
       AU.addPreserved<DependenceAnalysisWrapperPass>();
+      AU.addRequired<AAResultsWrapperPass>(); // 添加这一行
       getLoopAnalysisUsage(AU);
     }
   
@@ -375,22 +504,27 @@ namespace {
       auto &DT = getAnalysis<DominatorTreeWrapperPass>().getDomTree();
       auto &SE = getAnalysis<ScalarEvolutionWrapperPass>().getSE();
       DependenceInfo *depInfo = &getAnalysis<DependenceAnalysisWrapperPass>().getDI();
+      AliasAnalysis &AA = getAnalysis<AAResultsWrapperPass>().getAAResults(); // 获取别名分析实例
       
+
       // Only runs on the innermost loop
       if (L->getSubLoops().size() != 0) {
         return false;
       }
 
-      // resMII = max{Access_p / Port_p}
-      size_t resMII = calcResMII(L);
+      // errs() << "arrayConfig: " << arrayConfig << "\n";
+      auto arrayConfigMap = parseConfig(arrayConfig);
 
-      errs() << "\ncalculate resII succussfully! resII = " << resMII << "\n\n";
+      // resMII = max{Access_p / Port_p}
+      size_t resMII = calcResMII(L, arrayConfigMap);
+
+      // errs() << "\ncalculate resII succussfully! resII = " << resMII << "\n\n";
 
       //recMII = max{Delay_ins / Distance_ins}
-      size_t recMII = calcRecMII(L, LI, depInfo);
+      size_t recMII = calcRecMII(L, LI, depInfo, AA);
 
-      errs() << "resMII = " << resMII << "\n";
-      errs() << "recMII = " << recMII << "\n";
+      // errs() << "resMII = " << resMII << "\n";
+      // errs() << "recMII = " << recMII << "\n";
 
       unsigned II = std::max(resMII, recMII);
       errs() << "II = " << II << "\n";
